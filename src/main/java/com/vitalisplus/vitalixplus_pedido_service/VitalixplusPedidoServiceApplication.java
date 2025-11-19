b586424cfb9a4f783a39895d6e10af5e26e3052d
yeisonmenau <yeisonandresdh@gmail.com>
2025-11-18 23:34:47 -0500

    Estructura inicial

---
+++ src/main/java/com/vitalisplus/vitalixplus_pedido_service/VitalixplusPedidoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vitalisplus.vitalixplus_pedido_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalixplusPedidoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalixplusPedidoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitalisplus/vitalixplus_pedido_service/VitalixplusPedidoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalisplus.vitalixplus_pedido_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalixplusPedidoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
